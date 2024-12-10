ae1cadd4cf8e6c808bfa2fd93546871c1f8ce79d
TwoClocks <5883156+TwoClocks@users.noreply.github.com>
2024-12-10 08:16:42 -0800

    Add comment of issues to Main.kt

--- src/main/kotlin/ksp_bug/app/Main.kt
+++ src/main/kotlin/ksp_bug/app/Main.kt
@@ -1,11 +1,15 @@
-
 package ksp_bug.app
+
 import ksp_bug.annotation.CustomAnnotation
 
 @CustomAnnotation
 fun main(args: Array<String>) {
   println("Hello world")
 
+  // IntelliJ and Bazel can run this code fine,
+  // but IntelliJ can not find this class or the source
+  // code for it. It shows up as a red squiggly line.
+  // "Unresolved reference: GeneratedClass"
   val genCode = GeneratedClass()
   println("quote: ${genCode.quote}")
 }
